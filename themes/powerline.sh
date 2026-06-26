36a183f02d9fef167d6ef411e28e838214356661
Mashio Hasow <mediastatues@gmail.com>
2026-06-26 17:17:29 +0700

    Fix export syntax for color variables in powerline.sh

--- themes/powerline.sh
+++ themes/powerline.sh
@@ -4,15 +4,15 @@
 # shows git status and more
 
 # powerline colors
-export BLACK='\033[0;30m'
-export RED='\033[0;31m'
-export GREEN='\033[0;32m'
-export YELLOW='\033[1;33m'
-export BLUE='\033[0;34m'
-export PURPLE='\033[0;35m'
-export CYAN='\033[0;36m'
-export WHITE='\033[1;37m'
-export NC='\033[0m'
+export BLACK=$'\033[0;30m'
+export RED=$'\033[0;31m'
+export GREEN=$'\033[0;32m'
+export YELLOW=$'\033[1;33m'
+export BLUE=$'\033[0;34m'
+export PURPLE=$'\033[0;35m'
+export CYAN=$'\033[0;36m'
+export WHITE=$'\033[1;37m'
+export NC=$'\033[0m''
 
 # fancy ls colors
 export LS_COLORS='di=1;34:fi=0;37:ln=1;36:pi=0;33:so=1;35:bd=1;33:cd=1;33:or=1;31:ex=1;32'
